b6a0a1a879d2fc7ef756c56868ef31531f0b4892
rosaalwyh <alawiyahrosa@gmail.com>
2021-06-14 22:39:17 +0700

    memperbarui fitur jadwal

--- application/controllers/admin/dokter.php
+++ application/controllers/admin/dokter.php
@@ -1,65 +1,83 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /**
  * 
  */
 class Dokter extends CI_Controller
 {
-	
-	public function __construct(){
+
+	public function __construct()
+	{
 		parent::__construct();
 		$this->load->model('admin/model_dokter');
+		$this->load->model('admin/model_jadwal');
 		$this->load->model('m_poli');
 		$this->load->model('m_rs');
+		$this->load->model('admin/model_rs');
 		$this->load->library('form_validation');
 	}
 
-	public function index(){
+	public function index()
+	{
 		$data['title'] = 'Curiter | Admin';
 		$data['d'] = $this->model_dokter->get_dokter();
 		$data['rs'] = $this->model_dokter->get_rs();
-		$this->load->view('header_page_admin',$data);
-		$this->load->view('admin/v_rumahsakit',$data);
+		$this->load->view('header_page_admin', $data);
+		$this->load->view('admin/v_rumahsakit', $data);
 		$this->load->view('footer_page');
 	}
 
-	public function detaildokter($id){
+	public function detaildokter($id)
+	{
 		$data['title'] = 'Curiter | Admin';
 		$data['d'] = $this->model_dokter->get_dokterbyid($id);
 		$data['rs'] = $this->m_rs->get_datars($id);
 		$data['poli'] = $this->m_poli->get_polibyid($id);
-		$this->load->view('header_page_admin',$data);
-		$this->load->view('admin/v_datadokter',$data);
+		$this->load->view('header_page_admin', $data);
+		$this->load->view('admin/v_datadokter', $data);
 		$this->load->view('footer_page');
 	}
 
-	public function tambah(){
+	public function tambah()
+	{
 		$data = [
 			'id_rs' => $this->input->post('id_rs', true),
 			'no_dokter' => $this->input->post('no', true),
-			'nama_dokter' =>$this->input->post('nama', true),
-			'id_poli' => $this->input->post('id_poli',true),
+			'nama_dokter' => $this->input->post('nama', true),
+			'id_poli' => $this->input->post('id_poli', true),
 			'email_dokter' => $this->input->post('email', true)
 		];
 		$this->model_dokter->tambah_dokter($data);
-		redirect('admin/dokter/detaildokter/'.$data['id_rs']);
+		redirect('admin/dokter/detaildokter/' . $data['id_rs']);
 	}
-	public function edit($id){
+	public function edit($id)
+	{
 		$data = [
 			'id_rs' => $this->input->post('rs', true),
 			'no_dokter' => $this->input->post('no', true),
-			'nama_dokter' =>$this->input->post('nama', true),
-			'spesialis_dokter' => $this->input->post('spesialis',true),
-			'bio_dokter' => $this->input->post('bio',true),
+			'nama_dokter' => $this->input->post('nama', true),
+			'spesialis_dokter' => $this->input->post('spesialis', true),
+			'bio_dokter' => $this->input->post('bio', true),
 			'email_dokter' => $this->input->post('email', true),
 			'harga_dokter' => $this->input->post('harga', true)
 		];
 		$this->model_dokter->update_dokter($id, $data);
 		redirect('admin/dokter/index');
 	}
-	public function hapus($id){
+	public function hapus($id)
+	{
 		$this->model_dokter->delete_dokter($id);
 		redirect('admin/dokter/index');
+	}
+
+	public function jadwal($id)
+	{
+		$data['title'] = 'Curiter | Admin';
+		$data['d'] = $this->model_dokter->get_dokter();
+		$data['jadwal'] = $this->model_jadwal->getjadwal($id);
+		$this->load->view('header_page_admin', $data);
+		$this->load->view('admin/v_jadwal_dokter', $data);
+		$this->load->view('footer_page');
 	}
 }
--- application/views/admin/v_datadokter.php
+++ application/views/admin/v_datadokter.php
@@ -1,165 +1,174 @@
-  <!DOCTYPE html>
-  <html lang="" dir="ltr">
-    <head>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css') ?>">
-    <script src="<?php echo base_url('assets/js/jquery.min.js') ?>"></script>
-    <script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
-    <script src="<?php echo base_url('assets/js/all.min.js') ?>"></script>
-    <script src="<?=base_url('assets/js/fontawesome-all.js');?>">
-	</script>
+<!DOCTYPE html>
+<html lang="" dir="ltr">
 
-	<script type="text/javascript" src="<?=base_url('assets/js/bootstrap.min.js');?>">
-	</script>
-      <title>Curiter | Admin</title>
-      <style>
-        .bodiadm{
-          margin-top: 90px;
-        }
-        .button_tambah{
-    			background-color: #FFFFFF;
-    			color: #ec4638;
-    			border: 2px solid #ec4638;
-    			padding: 5px 10px;
-    			margin: 0px 0px 4px 0px;
-    			font-size: 15px;
-    			border-radius: 25px;
-    		}
-    		.button_tambah:hover{
-    			background-color: #ec4638;
-    			color: #FFFFFF;
-    		}
-      </style>
-    </head>
-    <body class="bodiadm">
-      <div class="container">
-        <div class="box">
-          <h3>Daftar Dokter</h3>
-          <br>
-          <button type="button" class="button_tambah" data-toggle="modal" data-target="#tambah">Tambah Dokter</button>
-          <br></br>
-          <table class="table table-bordered table-responsive" id="table">
-            <thead>
-              <tr>
-                <th>No</th>
-                <th>Foto</th>
-                <th>Nama Dokter</th>
-                <th>Spesialis</th>
-                <th>No Telp</th>
-                <th>Email</th>
-                <th>Edit</th>
-                <th>Hapus</th>
-              </tr>
-            </thead>
-            <tbody>
-              <?php $no=1;
-              foreach ($d as $d) {
-                    ?>
-              <tr>
-                <form>
-                  <td><?= $no++; ?></td>
-                  <td><img src="<?php echo base_url(); ?>/Assets/doctor.png" class="card-img" alt="..." ></td>
-                  <td><?php echo $d['nama_dokter']; ?></td>
-                  <td><?php echo $d['spesialis_dokter']; ?></td>
-                  <td><?php echo $d['no_dokter']; ?></td>
-                  <td><?php echo $d['email_dokter'] ?></td>
-                  <td><button type="button" class="btn btn-warning" data-toggle="modal" data-target="#edit<?= $d['id_dokter'] ?> "><i class="fas fa-edit"></i></button></td>
-                  <td><a href="<?= base_url(); ?>admin/dokter/hapus/<?= $d['id_dokter'] ?>" type="button" class="btn btn-danger" onClick="return confirm('Apakah Anda Yakin?')" ><i class="fas fa-trash"></i></a></td>
-                </form>
-                </tr> 
-                <!-- Modal Ubah Dokter -->
-                <div class="modal fade" id="edit<?= $d['id_dokter'] ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                  <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <h5 class="modal-title"><?="Update Data Dokter ".$d['nama_dokter']?></h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                          <span aria-hidden="true">&times;</span>
-                        </button>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css') ?>">
+  <script src="<?php echo base_url('assets/js/jquery.min.js') ?>"></script>
+  <script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
+  <script src="<?php echo base_url('assets/js/all.min.js') ?>"></script>
+  <script src="<?= base_url('assets/js/fontawesome-all.js'); ?>">
+  </script>
+
+  <script type="text/javascript" src="<?= base_url('assets/js/bootstrap.min.js'); ?>">
+  </script>
+  <title>Curiter | Admin</title>
+  <style>
+    .bodiadm {
+      margin-top: 90px;
+    }
+
+    .button_tambah {
+      background-color: #FFFFFF;
+      color: #365465;
+      border: 2px solid #365465;
+      padding: 5px 10px;
+      margin: 0px 0px 4px 0px;
+      font-size: 15px;
+      border-radius: 25px;
+    }
+
+    .button_tambah:hover {
+      background-color: #365465;
+      color: #FFFFFF;
+    }
+  </style>
+</head>
+
+<body class="bodiadm">
+  <div class="container">
+    <div class="box">
+      <h3>Daftar Dokter</h3>
+      <br>
+      <button type="button" class="button_tambah" data-toggle="modal" data-target="#tambah">Tambah Dokter</button>
+      <br></br>
+      <table class="table table-bordered table-responsive" id="table">
+        <thead>
+          <tr>
+            <!-- <?= var_dump($d); ?> -->
+            <th>No</th>
+            <th>Foto</th>
+            <th>Nama Dokter</th>
+            <th>Spesialis</th>
+            <th>No Telp</th>
+            <th>Email</th>
+            <th>Edit</th>
+            <th>Hapus</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php $no = 1;
+          foreach ($d as $d) {
+          ?>
+            <tr>
+              <form>
+                <td><?= $no++; ?></td>
+                <td><img src="<?php echo base_url(); ?>/Assets/doctor.png" class="card-img" alt="..."></td>
+                <td><a href="<?= base_url('admin/dokter/jadwal/' . $d['id_dokter']) ?>" style="color:black"><?php echo $d['nama_dokter']; ?></a></td>
+                <td><?php echo $d['id_poli']; ?></td>
+                <td><?php echo $d['no_dokter']; ?></td>
+                <td><?php echo $d['email_dokter'] ?></td>
+                <td><button type="button" class="btn btn-warning" data-toggle="modal" data-target="#edit<?= $d['id_dokter'] ?> "><i class="fas fa-edit"></i></button></td>
+                <td><a href="<?= base_url(); ?>admin/dokter/hapus/<?= $d['id_dokter'] ?>" type="button" class="btn btn-danger" onClick="return confirm('Apakah Anda Yakin?')"><i class="fas fa-trash"></i></a></td>
+              </form>
+            </tr>
+            <!-- Modal Tambah Dokter -->
+            <div class="modal fade" id="tambah" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel1" aria-hidden="true">
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <center>
+                      <h2>TAMBAH DATA DOKTER</h2>
+                    </center>
+                  </div>
+                  <div class="modal-body">
+                    <form method="POST" action="<?= base_url(); ?>admin/dokter/tambah">
+                      <input type="hidden" name="id_rs" value="<?= $rs['id_rs'] ?>">
+                      <div class="form-group">
+                        <label for="formGroupExampleInput">Nama Dokter</label>
+                        <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Nama Dokter" name="nama" required>
                       </div>
+                      <div class="form-group">
+                        <label for="exampleFormControlSelect2">Spesialis Dokter</label>
+                        <select multiple class="form-control" name="id_poli" id="exampleFormControlSelect2">
+                          <?php foreach ($poli as $pol) { ?>
+                            <option name value="<?php echo $pol['id_poli']; ?>"><?php echo $pol['nama_poli']; ?></option>
+                          <?php } ?>
+                        </select>
+                      </div>
+                      <div class="form-group">
+                        <label for="formGroupExampleInput">No Telp</label>
+                        <input type="text" class="form-control" id="formGroupExampleInput" placeholder="No Telp" name="no">
+                      </div>
+                      <div class="form-group">
+                        <label for="formGroupExampleInput">Email</label>
+                        <input type="email" class="form-control" id="formGroupExampleInput" placeholder="Email" name="email">
+                      </div>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                    <input type="submit" class="btn btn-primary" id="tambah" value="Submit" placeholder="Simpan">
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
 
-                        <div class="modal-body">
-                          <div class="boxsetting">
-                            <br>
-                            <form action="<?= base_url() ?>admin/dokter/edit/<?= $d['id_dokter'] ?>" method="post">
-                              <input type="hidden" name="id" value="<?= $d['id_dokter'] ?>">
-                              <div class="form-group">
-                                  <label for="formGroupExampleInput">No Telp</label>
-                                  <input type="text" class="form-control" id="formGroupExampleInput" placeholder="no" name="no"  value="<?php echo $d['no_dokter'] ?>">
-                              </div>
-                              <div class="form-group">
-                                  <label for="formGroupExampleInput">Nama Dokter</label>
-                                  <input type="text" class="form-control" id="formGroupExampleInput" placeholder="nama" name="nama"  value="<?php echo $d['nama_dokter'] ?>" required>
-                              </div>
-                              <div class="form-group">
-                                  <label for="formGroupExampleInput">Spesisalis</label>
-                                  <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Spesisalis" name="spesialis"  value="<?php echo $d['spesialis_dokter'] ?>" required>
-                              </div>
-                              <div class="form-group">
-                                <label for="formGroupExampleInput">Email</label>
-                                <input type="email" class="form-control" id="formGroupExampleInput" placeholder="Email" name="email"  value="<?php echo $d['email_dokter'] ?>">
-                              </div>
-                              <div class="form-group">
-                                <label for="formGroupExampleInput2">Rumah Sakit</label>
-                                <select class="form-control" id="formGroupExampleInput2" name="rs" required>
-                                <?php foreach ($rs as $r ) {?>
-                                    <option value="<?php echo $r->id_rs; ?>"><?php echo $r->nama_rs;  ?></option>
-                                <?php } ?>
-                                </select>            
-                              </div>
-                              <button type="submit" name="tambah" class="btn btn-primary float-right">Ubah Data</button>
-                            </form>
-                            <br>
-                          </div>
+            <!-- Modal Ubah Dokter -->
+            <div class="modal fade" id="edit<?= $d['id_dokter'] ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title"><?= "Update Data Dokter " . $d['nama_dokter'] ?></h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+
+                  <div class="modal-body">
+                    <div class="boxsetting">
+                      <br>
+                      <form action="<?= base_url() ?>admin/dokter/edit/<?= $d['id_dokter'] ?>" method="post">
+                        <input type="hidden" name="id" value="<?= $d['id_dokter'] ?>">
+                        <div class="form-group">
+                          <label for="formGroupExampleInput">No Telp</label>
+                          <input type="text" class="form-control" id="formGroupExampleInput" placeholder="no" name="no" value="<?php echo $d['no_dokter'] ?>">
                         </div>
+                        <div class="form-group">
+                          <label for="formGroupExampleInput">Nama Dokter</label>
+                          <input type="text" class="form-control" id="formGroupExampleInput" placeholder="nama" name="nama" value="<?php echo $d['nama_dokter'] ?>" required>
+                        </div>
+                        <div class="form-group">
+                          <label for="formGroupExampleInput">Spesisalis</label>
+                          <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Spesisalis" name="spesialis" value="<?php echo $d['spesialis_dokter'] ?>" required>
+                        </div>
+                        <div class="form-group">
+                          <label for="formGroupExampleInput">Email</label>
+                          <input type="email" class="form-control" id="formGroupExampleInput" placeholder="Email" name="email" value="<?php echo $d['email_dokter'] ?>">
+                        </div>
+                        <div class="form-group">
+                          <label for="formGroupExampleInput2">Rumah Sakit</label>
+                          <select class="form-control" id="formGroupExampleInput2" name="rs" required>
+                            <?php foreach ($rs as $r) { ?>
+                              <option value="<?php echo $r->id_rs; ?>"><?php echo $r->nama_rs;  ?></option>
+                            <?php } ?>
+                          </select>
+                        </div>
+                        <button type="submit" name="tambah" class="btn btn-primary float-right">Ubah Data</button>
+                      </form>
+                      <br>
                     </div>
                   </div>
                 </div>
-                    
-              <?php } ?>
-            </tbody>
-          </table>
-        </div>
-      </div>
-       <!-- Modal Tambah Dokter -->
-      <div class="modal fade" id="tambah" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel1" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-            <center><h2>TAMBAH DATA DOKTER</h2></center>
-            </div>
-            <div class="modal-body">
-            <form method="POST" action="<?= base_url(); ?>admin/dokter/tambah">
-              <input type="hidden" name="id_rs" value="<?= $rs['id_rs'] ?>">
-              <div class="form-group">
-                <label for="formGroupExampleInput">Nama Dokter</label>
-                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Nama Dokter" name="nama" required>
-              </div>
-              <div class="form-group">
-                <label for="exampleFormControlSelect2">Example multiple select</label>
-                <select multiple class="form-control" name="id_poli" id="exampleFormControlSelect2">
-                  <?php foreach ($poli as $pol ) {?>
-                    <option name value="<?php echo $pol['id_poli']; ?>"><?php echo $pol['nama_poli']; ?></option>
-                  <?php } ?>
-                </select>
-              </div>
-               <div class="form-group">
-                <label for="formGroupExampleInput">No Telp</label>
-                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="No Telp" name="no">
-              </div>
-              <div class="form-group">
-                <label for="formGroupExampleInput">Email</label>
-                <input type="email" class="form-control" id="formGroupExampleInput" placeholder="Email" name="email">
               </div>
             </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
-              <input  type="submit" class="btn btn-primary" id="tambah" value="Submit" placeholder="Simpan">
-            </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </body>
-  </html>
+
+          <?php } ?>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</body>
+
+</html>
