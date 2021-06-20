8ef925dd553f32159d85571d83864e0545129199
rosaalwyh <alawiyahrosa@gmail.com>
2021-06-20 17:56:49 +0700

    apus login user semua controller index

--- application/controllers/About.php
+++ application/controllers/About.php
@@ -1,16 +1,18 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class About extends CI_Controller {
-    public function __construct(){
+class About extends CI_Controller
+{
+    public function __construct()
+    {
         parent::__construct();
         $this->load->library('session');
         $this->load->model('m_user');
     }
-    public function index(){
-        $this->m_user->checklogin();
+    public function index()
+    {
         $data['title'] = "Curiter | About";
-        $this->load->view('header_page',$data);
+        $this->load->view('header_page', $data);
         $this->load->view('v_about');
         $this->load->view('footer_page');
     }
